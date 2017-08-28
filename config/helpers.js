1a9f90cdbf59cfda9251858d94111527733d15d8
Michaelmilk <gjx12358@gmail.com>
2017-08-29 07:55:38 +0800

    vendor.js

--- config/helpers.js
+++ config/helpers.js
@@ -2,6 +2,11 @@ var path = require('path');
 
 var _root = path.resolve(__dirname, '..');
 
+// console.log("__dirname", __dirname);
+// console.log("_root", _root);
+// __dirname D:\code\github\webpack\config
+// _root D:\code\github\webpack
+
 function root(args) {
   args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [_root].concat(args));
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 // import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 // import '../../node_modules/bootstrap/dist/js/bootstrap.js';
 //use the package name directly, no need to use relative path
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+//import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/js/bootstrap.js';
+
+//import '../assets/css/styles.css';
 
 @NgModule({
   imports: [
