740b1570a44715ec1535fdb3b4eb80ef815375d4
Michaelmilk <gjx12358@gmail.com>
2017-10-06 23:22:30 +0800

    define class property in constructor

--- src/app/core/experimentDto.ts
+++ src/app/core/experimentDto.ts
@@ -1,15 +1,17 @@
 import {AnalysisType} from './enums'
 
 export class experimentDto{
-    Id: number;
-    Name: string;
-    Type: AnalysisType;
-    CreatedBy: string;
-    UpdatedBy: string;
-    EntitySpaceName?: string;
-    EntitySpaceUrl?: string;
-    EntityViewName?: string;
-    EntityViewUrl?: string;
-    CustomerId?: string;
-    CustomerEnv?: string;
+    constructor(
+        public Id: number,
+        public Name: string,
+        public Type: AnalysisType,
+        public CreatedBy: string,
+        public UpdatedBy: string,
+        public EntitySpaceName?: string,
+        public EntitySpaceUrl?: string,
+        public EntityViewName?: string,
+        public EntityViewUrl?: string,
+        public CustomerId?: string,
+        public CustomerEnv?: string,
+      ) {  }
 }
