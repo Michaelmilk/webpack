ad12fc58f1e4f5edf03b3bdf7d1ccab0a41ff61a
jixge <jixge@microsoft.com>
2017-11-07 18:04:01 +0800

    fix valuable name

--- src/app/components/triage/triage.component.ts
+++ src/app/components/triage/triage.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class TriageComponent implements OnInit {
     customerEnvironment: string;
-    entitySpaceViewName: string;
+    customerEnvironment: string;
 
     constructor() { }
 
