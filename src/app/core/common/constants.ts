fd65848369deee6b611ae0fac0b3acc89bcc8f72
jixge <jixge@microsoft.com>
2017-11-09 18:15:50 +0800

    add entityview structure

---
+++ src/app/core/common/constants.ts
@@ -0,0 +1,13 @@
+export class Constants{
+
+    //triage debug folder stream
+    public static streamValueChurnCounter:string = "ValueChurnCounter.ss";
+    public static streamPipelineTopEntitiesChurnCounter:string = "PipelineTopEntitiesChurnCounter.ss";
+    public static streamPipelineChurnCounter:string = "PipelineChurnCounter.ss";
+    public static streamPipelineTypeChurnCounter:string = "PipelineTypeChurnCounter.ss";
+    public static streamPipelineUpdatedEntitiesChurnCounterStreamName:string = "PipelineUpdatedEntitiesChurnCounter.ss";
+
+
+    public static streamView:string = "View.Combined.ss";
+    public static streamEntitySpace:string = "Fact.Base.ss";
+}
--- src/app/core/triage/entitySpaceView.ts
+++ src/app/core/triage/entitySpaceView.ts
@@ -1,18 +1,71 @@
+import { Constants } from '../../core/common/constants'
 
+
+export class MappingFile{
+    constructor(
+        public name:string,
+        public version: string//dot splited
+    ){}
+}
+
+export class MappingSetting{
+    constructor(
+        public mappingFiles: MappingFile[],
+        public functoids: string[]
+    ){}
+}
+
+export class EntitySpaceViewVersion{
+    // public _viewStreamPath: string;
+    // public _entitySpaceStreamPath: string;
+
+    constructor(
+        public state: string,
+        public virtualCluster: string,
+        public vCRelativeFolder: string,
+        public mappingSetting: MappingSetting,
+        public lastUpdatedTime: string,        
+    ){}
+    
+    get viewStreamPath(): string{
+        if (!this.virtualCluster || !this.vCRelativeFolder){
+            return "";
+        }
+
+        return `${this.virtualCluster}${this.vCRelativeFolder}${Constants.streamView}`;
+    }
+    
+    get entitySpaceStreamPath(): string{
+        if (!this.virtualCluster || !this.vCRelativeFolder){
+            return "";
+        }
+
+        let len = this.vCRelativeFolder.length;
+        let entitySpaceFolderslashPos = this.vCRelativeFolder.substr(0, len - 1).lastIndexOf("/");
+        let entitySpaceRelativeFolder = this.vCRelativeFolder.substr(0, entitySpaceFolderslashPos + 1);
+        return `${this.virtualCluster}${entitySpaceRelativeFolder}${Constants.streamEntitySpace}`;
+    }
+}
 
 export class EntitySpaceView{
     constructor(
-        public id: number,
+        public viewKey: string,
         public name: string,
+        public model: string,
+        public state: string,
         public createdBy: string,
         public createdTime: string,
         public updatedBy: string,
         public updatedTime: string,
-        public entitySpaceName?: string,
-        public entitySpaceUrl?: string,
-        public entityViewName?: string,
-        public entityViewUrl?: string,
+        public entitySpaceViewVersion: EntitySpaceViewVersion,
         public customerId?: string,
         public customerEnv?: string,
+        public entitySpaceName?: string,
+        public entitySpaceUrl?: string,
+        public entitySpaceStreamPath?: string,
+        public entitySpaceDebugStreamFolder?: string,
+        public entityViewName?: string,
+        public entityViewUrl?: string,
+        public entityViewStreamPath?: string
       ) {  }
 }
