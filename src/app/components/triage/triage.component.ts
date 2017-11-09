d90cd425b78a621ef4817d815f528812fc480e29
Michaelmilk <gjx12358@gmail.com>
2017-11-09 23:52:00 +0800

    replace with readonly and churn category

--- src/app/components/triage/triage.component.ts
+++ src/app/components/triage/triage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EntitySpaceView } from '../../core/triage/entitySpaceView';
+
 @Component({
     selector: 'triage',
     templateUrl: './triage.component.html',
@@ -7,14 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TriageComponent implements OnInit {
-    customerEnvironment: string;
-    entitySpaceViewName: string;
+    customerIdEnvironment: string;
+    entitySpaceViewKey: string;
+    entitySpaceView: EntitySpaceView;
+    
 
     constructor() { }
 
     ngOnInit() { }
 
     loadEntitySpaceView(){
-        
+        this.parseEntitySpaceViewmeta();
+    }
+
+    parseEntitySpaceViewmeta() {
+        let params= this.customerIdEnvironment.split("-");
+        if (params.length >= 2)
+        {
+            let customerId: string = params[0];
+            let customerEnv: string  = params[1];
+            let pos = this.entitySpaceViewKey.indexOf("_");
+            let entitySpaceName: string = this.entitySpaceViewKey.substr(0, pos);
+            let entityViewName: string = this.entitySpaceViewKey.substr(pos + 1);
+            this.entitySpaceView = new EntitySpaceView(this.entitySpaceViewKey, customerId, customerEnv, entityViewName, entitySpaceName);
+        }
+        console.log(this.entitySpaceView);
     }
 }
--- src/app/core/common/constants.ts
+++ src/app/core/common/constants.ts
@@ -1,13 +1,13 @@
 export class Constants{
 
     //triage debug folder stream
-    public static streamValueChurnCounter:string = "ValueChurnCounter.ss";
-    public static streamPipelineTopEntitiesChurnCounter:string = "PipelineTopEntitiesChurnCounter.ss";
-    public static streamPipelineChurnCounter:string = "PipelineChurnCounter.ss";
-    public static streamPipelineTypeChurnCounter:string = "PipelineTypeChurnCounter.ss";
-    public static streamPipelineUpdatedEntitiesChurnCounterStreamName:string = "PipelineUpdatedEntitiesChurnCounter.ss";
+    public static readonly streamValueChurnCounter:string = "ValueChurnCounter.ss";
+    public static readonly streamPipelineTopEntitiesChurnCounter:string = "PipelineTopEntitiesChurnCounter.ss";
+    public static readonly streamPipelineChurnCounter:string = "PipelineChurnCounter.ss";
+    public static readonly streamPipelineTypeChurnCounter:string = "PipelineTypeChurnCounter.ss";
+    public static readonly streamPipelineUpdatedEntitiesChurnCounterStreamName:string = "PipelineUpdatedEntitiesChurnCounter.ss";
 
 
-    public static streamView:string = "View.Combined.ss";
-    public static streamEntitySpace:string = "Fact.Base.ss";
+    public static readonly streamView:string = "View.Combined.ss";
+    public static readonly streamEntitySpace:string = "Fact.Base.ss";
 }
